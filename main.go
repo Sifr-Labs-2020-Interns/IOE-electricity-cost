0b2881dde62d74089cda0b1c3af330a608f6f327
akilan <akilanselva@hotmail.com>
2020-06-15 00:42:07 +0400

    code template for adduser , removeuser , addtransaction

--- main.go
+++ main.go
@@ -15,18 +15,91 @@ type NewUser struct {
     Admid_key string `form: "admin_key" binding:"Required"`
 }
 
+//Post request parameters for route to remove user
+type RemoveUser struct {
+    Username string `form: "username" binding:"Required"`,
+    Email_Id string `form: "email_id" binding:"Required"`,
+    Admid_key string `form: "admin_key" binding:"Required"`
+}
 
+//Post request parameters for route to Add transaction
+type AddTransaction struct {
+    User_key string `form: "admin_key" binding:"Required"`,
+    Watts string `form: "Watts" binding:"Required"`,
+    Type string `form: "Type" binding:"Required"`
+}
 
 func main() {
-    // All routes
+
     m := macaron.Classic()
     // Public files
     m.Use(macaron.Static("public"))
+
+    // All routes
     m.Post("/adduser",binding.Bind(NewUser{}),adduser)
+    m.Post("/removeuser",binding.Bind(RemoveUser{}),removeuser)
+    m.Post("/addtransaction",binding.Bind(AddTransaction{}),addtransaction)
+
     m.Run()
 }
 
+// TODO: add a new user
 func adduser(ctx *macaron.Context,newuser NewUser){
-   fmt.Printf("%v\n", newuser.Name)
+
+   // Get user from post request
+   name := newuser.Name
+   Username := newuser.Username
+   Email_id := newuser.Email_Id
+   Admin_key := newuser.Admin_key
+
+   /* Check if the admin key is valid
+        |___ is valid
+             |___ Generate key for user and insert data to the database and return JSON success with
+                  user key
+        |___ not valid
+             |___ Returns in json error admin key not valid
+
+   */
+
+   // Remove this when you have your JSON return statementa
    ctx.Resp.WriteHeader(200)
+}
+
+// TODO: remove user
+func removeuser(ctx *macaron.Context,removeuser RemoveUser){
+
+  // Get user information from post request
+  Username := removeuser.Username
+  Email_id := removeuser.Email_Id
+  Admin_key := removeuser.Admin_key
+
+  /* Check if the admin key is valid
+       |___ is valid
+            |___ Remove user from database and return success
+       |___ not valid
+            |___ Returns in json error admin key not valid
+  */
+
+  // Remove this when you have your JSON return statementa
+  ctx.Resp.WriteHeader(200)
+}
+
+// TODO: Add transaction
+func addtransaction(ctx *macaron.Context,addtransaction AddTransaction){
+
+  // Get information to add transaaction
+  User_key := addtransaction.User_key
+  Watts := addtransaction.Watts
+  Type := addtransaction.Type
+
+  /* Check if the user key is valid
+       |___ is valid
+            |___ Add the transaction information in the database and return JSON success
+       |___ not valid
+            |___ Returns in json error user key not valid
+  */
+
+  // Remove this when you have your JSON return statementa
+  ctx.Resp.WriteHeader(200)
+
 }
