cfba0cd0150fbb4365a1f7b79152f16830e689fa
akilan <akilanselva@hotmail.com>
2020-06-15 00:49:50 +0400

    pushed instructions

--- main.go
+++ main.go
@@ -3,29 +3,28 @@ package main
 
 import (
   "gopkg.in/macaron.v1"
-  "fmt"
   "github.com/go-macaron/binding"
 )
 
 //Post request parameters for route add user
 type NewUser struct {
-    Name  string `form:"name" binding:"Required"`,
-    Username string `form: "username" binding:"Required"`,
-    Email_Id string `form: "email_id" binding:"Required"`,
-    Admid_key string `form: "admin_key" binding:"Required"`
+    Name  string `form:"name" binding:"Required"`
+    Username string `form: "username" binding:"Required"`
+    Email_Id string `form: "email_id" binding:"Required"`
+    Admin_key string `form: "admin_key" binding:"Required"`
 }
 
 //Post request parameters for route to remove user
 type RemoveUser struct {
-    Username string `form: "username" binding:"Required"`,
-    Email_Id string `form: "email_id" binding:"Required"`,
-    Admid_key string `form: "admin_key" binding:"Required"`
+    Username string `form: "username" binding:"Required"`
+    Email_Id string `form: "email_id" binding:"Required"`
+    Admin_key string `form: "admin_key" binding:"Required"`
 }
 
 //Post request parameters for route to Add transaction
 type AddTransaction struct {
-    User_key string `form: "admin_key" binding:"Required"`,
-    Watts string `form: "Watts" binding:"Required"`,
+    User_key string `form: "admin_key" binding:"Required"`
+    Watts string `form: "Watts" binding:"Required"`
     Type string `form: "Type" binding:"Required"`
 }
 
@@ -46,11 +45,12 @@ func main() {
 // TODO: add a new user
 func adduser(ctx *macaron.Context,newuser NewUser){
 
-   // Get user from post request
-   name := newuser.Name
+   // Get user from post request - UNCOMMENT WHEN USING THE VARIABLES
+
+   /* name := newuser.Name
    Username := newuser.Username
    Email_id := newuser.Email_Id
-   Admin_key := newuser.Admin_key
+   Admin_key := newuser.Admin_key */
 
    /* Check if the admin key is valid
         |___ is valid
@@ -68,10 +68,10 @@ func adduser(ctx *macaron.Context,newuser NewUser){
 // TODO: remove user
 func removeuser(ctx *macaron.Context,removeuser RemoveUser){
 
-  // Get user information from post request
-  Username := removeuser.Username
+  // Get user information from post request - UNCOMMENT WHEN USING THE VARIABLES
+  /* Username := removeuser.Username
   Email_id := removeuser.Email_Id
-  Admin_key := removeuser.Admin_key
+  Admin_key := removeuser.Admin_key */
 
   /* Check if the admin key is valid
        |___ is valid
@@ -87,10 +87,10 @@ func removeuser(ctx *macaron.Context,removeuser RemoveUser){
 // TODO: Add transaction
 func addtransaction(ctx *macaron.Context,addtransaction AddTransaction){
 
-  // Get information to add transaaction
-  User_key := addtransaction.User_key
+  // Get information to add transaaction - UNCOMMENT WHEN USING THE VARIABLES
+  /* User_key := addtransaction.User_key
   Watts := addtransaction.Watts
-  Type := addtransaction.Type
+  Type := addtransaction.Type */
 
   /* Check if the user key is valid
        |___ is valid
