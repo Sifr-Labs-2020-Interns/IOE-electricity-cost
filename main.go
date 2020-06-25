891dbfe7633bbdaa33352156d2f4056748de23a3
Wesley Barreto <wgb1@hw.ac.uk>
2020-06-25 15:41:18 +0400

    Changed the import string of the connection package in main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"unicode"
 	"unsafe"
 
-	"./connection"
+	"github.com/Sifr-Labs-2020-Interns/IOE-electricity-cost/connection"
 	"golang.org/x/crypto/bcrypt"
 
 	"github.com/go-macaron/binding"
